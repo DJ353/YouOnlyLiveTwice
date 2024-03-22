1374051bcee5ab583ab1e18bf0b7e9f0a2a20395
Eldinur <134613763+StackAllocator@users.noreply.github.com>
2024-03-22 21:55:33 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
-#include <raylib.h>
-#include <iostream>
+#include "raylib.h"
+#include "window.hpp"
 
 int main() {
-    int screenWidth = 1920;
-    int screenHeight = 1080;
-    
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "You Only Live Twice");
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        EndDrawing();
-    }
+  constexpr int screenWidth = 1920;
+  constexpr int screenHeight = 1080;
+    
+  Display::Window window = Display::Window(screenWidth, screenHeight, "You Only Live Twice");
+  window.run();
+
+  return 0;
 }
