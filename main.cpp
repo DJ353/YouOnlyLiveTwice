39c2883dc93dc49f141147bb57b6741748b5368b
Daniel Jodas <djodas@gmx.de>
2024-03-22 21:30:22 +0100

    Window name

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     int screenHeight = 1080;
     
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "Collatz Conjecture Visualization");
+    InitWindow(screenWidth, screenHeight, "You Only Live Twice");
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
     while (!WindowShouldClose()) {
