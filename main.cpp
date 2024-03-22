44c6108538696af95772d6c4cb0231537020b3ee
Daniel Jodas <djodas@gmx.de>
2024-03-22 21:29:30 +0100

    window

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!!!";
+    int screenWidth = 1920;
+    int screenHeight = 1080;
+    
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "Collatz Conjecture Visualization");
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
 }
